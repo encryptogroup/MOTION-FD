33f8621dcdae5b0178983cd0e6603a9bb34e0471
oliver-schick <oliver.schick92@gmail.com>
2023-10-26 14:17:19 +0200

    Added copyright notice, updated submodules, added CODEOWNERS and LICENSE

--- src/motioncore/protocols/swift/swift_wire.cpp
+++ src/motioncore/protocols/swift/swift_wire.cpp
@@ -1,3 +1,27 @@
+// MIT License
+//
+// Copyright (c) 2023 Oliver Schick
+// Cryptography and Privacy Engineering Group (ENCRYPTO)
+// TU Darmstadt, Germany
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "swift_wire.h"
 
 namespace encrypto::motion::proto::swift {
@@ -41,8 +65,6 @@ template class MatrixWire<std::uint16_t>;
 template class MatrixWire<std::uint32_t>;
 template class MatrixWire<std::uint64_t>;
 template class MatrixWire<__uint128_t>;
-
-
 
 BooleanWire::BooleanWire(
   Backend& backend, BitVector<> values, 
